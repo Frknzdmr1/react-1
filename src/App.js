1734cae72c7f7dc0f95faa62faeef23c67cc9f1c
Frknzdmr1 <ozdemirf97@icloud.com>
2023-02-13 22:04:52 -0500

    template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mainSection'>
+      <h1>hello</h1>
+      <div className='body'>Main Body</div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae perspiciatis doloribus nostrum tempore quia voluptatibus aliquid. Optio dolorem autem, at accusamus nesciunt modi. Nisi fugiat nostrum, illum architecto commodi ipsum.</p>
     </div>
   );
 }
